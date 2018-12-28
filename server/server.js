0607a31ace51f3bd4aa82fb5c828a702d345c4cd
duang <niceduang@qq.com>
2018-12-28 17:49:22 +0800

    proxy

--- server/server.js
+++ server/server.js
@@ -1,17 +1,44 @@
-const express = require('express')
-const proxy = require('http-proxy-middleware')
+var path = require('path')
+var express = require('express')
+// 代理
+var proxy = require('http-proxy-middleware')
+// 跨域
+// var cors = require('cors')
 
-const app = express()
+var app = express()
+// app.use(cors())
 
-app.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', 'true')
-})
+app.use(express.static(path.join(__dirname, 'www')))
+
+// // 设置代理
+
+// // http://localhost:3000/v2/music/search?q=张杰 -> https://api.douban.com/v2/music/search?q=张杰
+// app.use('/v2', proxy({
+//   target: 'https://api.douban.com',
+//   changeOrigin: true
+// }))
+
+// // http://localhost:3000/getPlayerGloryRank?appid=10005&seasonid=KPL2018S1 -> https://api.tgatv.qq.com/app/match/getPlayerGloryRank?appid=10005&seasonid=KPL2018S1
+// app.use('/getPlayerGloryRank', proxy({
+//   target: 'https://api.tgatv.qq.com/app/match',
+//   changeOrigin: true
+// }))
+
+// app.use('/api', proxy({
+//   target: 'https://www.tuling123.com/openapi/',
+//   changeOrigin: true
+// }))
+
+// 设置代理
 
 app.middleware = [
-  proxy(['/getInfo'], {target: 'http://www.tuling123.com/openapi/api', changeOrigin: true}),
-  // proxy(['/sendData'], {target: '', changeOrigin: true})
+  proxy(['/v2'], {target: 'https://api.douban.com', changeOrigin: true}),
+  proxy(['/getPlayerGloryRank'], {target: 'https://api.tgatv.qq.com/app/match', changeOrigin: true}),
+  proxy(['/api'], {target: 'https://www.tuling123.com/openapi/', changeOrigin: true})
 ]
 
 app.use(app.middleware)
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
